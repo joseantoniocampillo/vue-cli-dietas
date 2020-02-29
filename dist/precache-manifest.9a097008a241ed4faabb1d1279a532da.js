f4a36c720acb05f28d0ce4bdf4483e48d4507e91
jose campi pi4 <jose@campi.pi4>
2020-02-29 11:22:37 +0000

    initial commit

---
+++ dist/precache-manifest.9a097008a241ed4faabb1d1279a532da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "6464b0d25497be9d50ea",
+    "url": "js/chunk-vendors.aa986ccd.js"
+  },
+  {
+    "revision": "d43769bd374a8657ead0",
+    "url": "js/app.9e6ef044.js"
+  },
+  {
+    "revision": "ee568f1fd7cb190e6f6fe996560c5845",
+    "url": "index.html"
+  },
+  {
+    "revision": "36938bbe277961386e7e309dcb30cbd8",
+    "url": "img/spain.36938bbe.svg"
+  },
+  {
+    "revision": "0a03308b30adef1009e8180798d592fe",
+    "url": "img/redondo_europa.0a03308b.svg"
+  },
+  {
+    "revision": "6464b0d25497be9d50ea",
+    "url": "css/chunk-vendors.257c6d34.css"
+  },
+  {
+    "revision": "d43769bd374a8657ead0",
+    "url": "css/app.ec0c187e.css"
+  }
+];
